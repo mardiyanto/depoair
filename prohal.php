2aa96c11eb9d197b2cbe00c628e8f8f0e6623d4e
mardiyanto <mardybest@gmail.com>
2023-07-05 10:04:44 +0700

    fix

--- prohal.php
+++ prohal.php
@@ -93,44 +93,41 @@
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
                     <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">Contact</p>
-                    <h1 class="display-5 mb-4">If You Have Any Query, Please Contact Us</h1>
-                    <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                    <form>
-                        <div class="row g-3">
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                    <label for="name">Your Name</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                    <label for="email">Your Email</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                    <label for="subject">Subject</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
-                                    <label for="message">Message</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <button class="btn btn-primary py-3 px-5" type="submit">Send Message</button>
-                            </div>
+                    <h1 class="display-5 mb-4">Jika anda butuh pertanyaan, silahkan hubungi kami </h1>
+                    <p class="mb-4"> </p>
+                    <h3 class="mb-4">Informasi Konta</h3>
+                    <div class="d-flex border-bottom pb-3 mb-3">
+                        <div class="flex-shrink-0 btn-square bg-secondary rounded-circle">
+                            <i class="fa fa-map-marker-alt text-body"></i>
                         </div>
-                    </form>
+                        <div class="ms-3">
+                            <h6>alamat kantor</h6>
+                            <span><?php echo"$k_k[alamat]";?></span>
+                        </div>
+                    </div>
+                    <div class="d-flex border-bottom pb-3 mb-3">
+                        <div class="flex-shrink-0 btn-square bg-secondary rounded-circle">
+                            <i class="fa fa-phone-alt text-body"></i>
+                        </div>
+                        <div class="ms-3">
+                            <h6>WA/hp</h6>
+                            <span><?php echo"$k_k[alias]";?></span>
+                        </div>
+                    </div>
+                    <div class="d-flex border-bottom-0 pb-3 mb-3">
+                        <div class="flex-shrink-0 btn-square bg-secondary rounded-circle">
+                            <i class="fa fa-envelope text-body"></i>
+                        </div>
+                        <div class="ms-3">
+                            <h6>Email</h6>
+                            <span><?php echo"$k_k[alias]";?></span>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style="min-height: 450px;">
                     <div class="position-relative rounded overflow-hidden h-100">
                         <iframe class="position-relative w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                        src="<?php echo"$k_k[peta]";?>"
                         frameborder="0" style="min-height: 450px; border:0;" allowfullscreen="" aria-hidden="false"
                         tabindex="0"></iframe>
                     </div>
